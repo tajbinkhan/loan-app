0aa5465e9fc69b81c7e2a3a44d3431165aec6c83
tajbinkhan <tajbink@gmail.com>
2022-08-15 12:25:42 +0600

    Host Updated

--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'loanapp.pythonanywhere.com']
 
 
 # Application definition
