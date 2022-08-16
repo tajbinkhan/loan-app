e4b94627b71dee5d4a83de88c6a060a47a70e608
tajbinkhan <tajbink@gmail.com>
2022-08-16 21:47:21 +0600

    404 Page Add

--- core/views.py
+++ core/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+def error(request, exception):
+	template_name = '404.html'
+	context = {}
+	return render(request, template_name, context)
+
 def privacy_policy(request):
 	template_name = 'policies/privacy_policy.html'
 	context = {}
