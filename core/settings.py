9183da434a85d79de318cb6ceeab4d086307394b
tajbinkhan <tajbink@gmail.com>
2022-09-01 13:19:06 +0600

    Updated Setting

--- core/settings.py
+++ core/settings.py
@@ -104,24 +104,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-if DEBUG:
-	DATABASES = {
-		'default': {
-			'ENGINE': 'django.db.backends.sqlite3',
-			'NAME': BASE_DIR / 'db.sqlite3',
-		}
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': BASE_DIR / 'db.sqlite3',
 	}
-else:
-	DATABASES = {
-		'default': {
-			'ENGINE': 'django.db.backends.postgresql',
-			'NAME': config('PGDATABASE'),
-			'USER': config('PGUSER'),
-			'PASSWORD': config('PGPASSWORD'),
-			'HOST': config('PGHOST'),
-			'PORT': config('PGPORT'),
-		}
-	}
+}
 
 
 # Password validation
