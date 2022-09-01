3b7118227b8e5b80ec48e97b2ddb31a4775cd318
tajbinkhan <tajbink@gmail.com>
2022-09-01 11:09:08 +0600

    Database Updated

--- core/settings.py
+++ core/settings.py
@@ -104,12 +104,24 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+	DATABASES = {
+		'default': {
+			'ENGINE': 'django.db.backends.sqlite3',
+			'NAME': BASE_DIR / 'db.sqlite3',
+		}
 	}
-}
+else:
+	DATABASES = {
+		'default': {
+			'ENGINE': config('DATABASE_URL'),
+			'NAME': config('PGDATABASE'),
+			'USER': config('PGUSER'),
+			'PASSWORD': config('PGPASSWORD'),
+			'HOST': config('PGHOST'),
+			'PORT': config('PGPORT'),
+		}
+	}
 
 
 # Password validation
